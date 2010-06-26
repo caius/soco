77ec3494c396cc21a88fdcdaf7715062a905765c
Caius Durling <git@caius.name>
2010-06-26 05:33:16 -0700

    Fixing some PHP Notice's.

--- htdocs/config.php
+++ htdocs/config.php
@@ -3,7 +3,7 @@
 # Select which theme to use
 # note: just set this to the folder name in ./content
 $theme = "default";
-define(THEME, $theme);
+define("THEME", $theme);
 
 # Setup some paths
 $_PATHS["base"]      = dirname(dirname(__FILE__)) . "/";
@@ -17,12 +17,12 @@ $_PATHS["logs"]      = $_PATHS["base"] . "logs/";
 ini_set("include_path", "$_PATHS[includes]:$_PATHS[pear]:$_PATHS[templates]");
 
 # define some basic paths
-define(BASE, $_PATHS["base"]);
-define(CONTENT, $_PATHS["content"]);
+define("BASE", $_PATHS["base"]);
+define("CONTENT", $_PATHS["content"]);
 
 # Are we using pretty URLs?
 $pretty_urls = true;
-if ($pretty_urls) { define(QUERYSTRING, ''); } else { define(QUERYSTRING, '?p='); }
+if ($pretty_urls) { define("QUERYSTRING", ''); } else { define("QUERYSTRING", '?p='); }
 
 # Carry on through
 include 'prepend.php';
